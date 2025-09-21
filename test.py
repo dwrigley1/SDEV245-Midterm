543b3520afd8ddbf9facd512f038aaa2840b3423
dwrigley1 <dwrigley1@ivytech.edu>
2025-09-21 11:26:48 -0400

    removed test file

--- test.py
+++
@@ -1,7 +0,0 @@
-# Using string concatenation
-print('Hello' + 'World' + '\n' + 'This is a test.')
-
-# Using f-strings (Python 3.6+)
-name = 'World'
-message = 'This is a test.'
-print(f'Hello{name}\n{message}')
